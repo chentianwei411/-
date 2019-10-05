432ff229fe765f61e8a4b76686cd491aa5a7aea3
ctw <ctw.3@163.com>
2019-10-05 12:09:18 +0800

    update shell_sort

--- shell_sort.rb
+++ shell_sort.rb
@@ -1,23 +1,49 @@
 def shell_sort(arr)
-  # gap从n/2开始，每轮除以2，最后一轮gap == 1
   gap = arr.length / 2
   while gap > 0
     i = gap
-    # 每轮gap：i从gap,加1，直到数组结束。
-    # 每个元素都和它同组内的位于它前面的元素进行比较。
-    # 分组后的每个组的第一元素，无需再遍历了。因为每组的第2个元素会和它进行比较。
     while i < arr.length
-      temp = arr[i]
       # 插入排序
+      temp = arr[i]
       j = i
       while j >= gap && arr[j - gap] > temp
         arr[j] = arr[j-gap]
+        arr[j - gap] = nil
         j -= gap
       end
       arr[j] = temp
+
       i += 1
     end
     gap = gap / 2
+  end
+  return arr
+end
+
+# 便于理解shell_sort的代码：上面的方法是更优化的算法。
+def shell_sort1(arr)
+  gap = arr.length/2
+  # 使用的是n除以2的i次方的步距。最后一轮gap等于1.
+  while gap > 0
+    i = gap
+    # 遍历从i开始的元素， i前面的元素无需遍历。因为插入排序法，从后往前比较。
+    while i < arr.length
+      # 设置指针j, 指针是要前移的。
+      # 相当于在队尾插入一个新元素，然后和前面的同组相邻元素比较和交换位置。
+      # 如此反复直到该元素找到确定位置。
+      j = i
+      while j >= gap && arr[j - gap] > arr[j]
+        # 被分到同组的相邻元素交换位置
+        temp = arr[j]
+        arr[j] = arr[j - gap]
+        arr[j - gap ] = temp
+        # 指针前移一个位置
+        # 此时，arr[j]位置的值是插入的元素，它会在下轮循环和前面的元素比较。
+        j = j - gap
+      end
+      i += 1
+    end
+    gap = gap/2
   end
   return arr
 end
